0f93b47778851bb99261dbb354e73cda7750585d
fadeevaelena <lena.kom-fadeeva@yandex.ru>
2022-11-29 23:49:18 +0300

    Добавили методы для класса Employee

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import datetime as dt
+
+
+def str_to_date(self_date, other_date):
+    dt1 = self_date.split('.')   # ['27','12', '1998']
+    dt2 = other_date.split('.')
+    self_date = dt.date(int(dt1[2]), int(dt1[1]), int(dt1[0]))
+    other_date = dt.date(int(dt2[2]), int(dt2[1]), int(dt2[0]))
+    return self_date, other_date
+    
+
 class Employee:
     def __init__(self, number, fio, bdate, oklad, on_leave = False):
         self.number = number
@@ -13,5 +24,25 @@ class Employee:
        return f"Сотрудник {self.number} {self.fio} {self.bdate} оклад: {self.oklad}, в отпуске: " \
               f"{'да' if self.on_leave else 'нет'}"
 
-petrov = Employee(1,"Петров А.А","12.12.2000",50000)
-print(petrov)
+    def __lt__(self, other):  # <
+        self_bdate, other_bdate = str_to_date(self.bdate, other.bdate)
+        return self_bdate < other_bdate
+
+    def __eq__(self, other):  # ==
+        self_bdate, other_bdate = str_to_date(self.bdate, other.bdate)
+        return self_bdate == other_bdate
+
+    def __le__(self, other): # <=
+        if self.__eq__(other):
+            return  True
+        if self.__lt__(other):
+            return True
+        return False
+
+
+petrov = Employee(1,"Петров А.А","12.12.1998",50000)
+smirnoff = Employee(1,"Смирнов П.Р","22.04.1999",546000)
+print(petrov > smirnoff)
+print(petrov >= smirnoff)
+print(petrov != smirnoff)
+
